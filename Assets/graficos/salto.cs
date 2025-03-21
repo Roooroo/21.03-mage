a0beaeae2c9b44b7781e15a9b208dc57c68d1351
Roooroo <rotoledo66@gmail.com>
2025-03-21 13:55:36 +0100

    primer commit

---
+++ Assets/graficos/salto.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class salto : MonoBehaviour
+{/* 
+    /* // Start is called before the first frame update
+    void Start()
+    {
+    Debug.Log("Hola Mundo!");
+    } */
+
+    // Update is called once per frame
+    /*
+    /*///
+    int miEdad = 4.1f;
+    float miAltura = 4.1f;
+    string = miNombre = "roero";
+    bool tengoMelena = false;
+
+    void Update()
+    {
+        Debug.Log("hola mundo");
+        miAltura
+    }
+}
+
